aaf665b536a7d32a2535b606d6752b62e1491d44
rosskell <59850988+rosskell@users.noreply.github.com>
2026-03-05 13:31:24 -0700

    Replace binary logo with text-based SVG asset

---
+++ ExclusionEngine.Web/Login.aspx.designer.cs
@@ -0,0 +1,11 @@
+namespace ExclusionEngine.Web
+{
+    public partial class Login
+    {
+        protected global::System.Web.UI.WebControls.Label ErrorMessage;
+        protected global::System.Web.UI.WebControls.TextBox UsernameTextBox;
+        protected global::System.Web.UI.WebControls.TextBox PasswordTextBox;
+        protected global::System.Web.UI.WebControls.Button LoginButton;
+        protected global::System.Web.UI.WebControls.HyperLink ForgotPasswordLink;
+    }
+}
